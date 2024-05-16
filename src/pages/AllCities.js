845bfd649af5ea8498d614e193c93ac74994d51a
MartinHolts <martinholtsmeier@gmail.com>
2024-05-16 20:45:49 +0300

    Added 5 cities to the API

--- src/pages/AllCities.js
+++ src/pages/AllCities.js
@@ -6,15 +6,23 @@ const AllCities = () => {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
+				/*
 				const response = await fetch(
 					"https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php"
 				);
+				*/
 
 				/*
 				const response = await fetch(
 					"https://mocktarget.apigee.net/xml"
 				);
 				*/
+
+				const response = await fetch(
+					//"https://api.openweathermap.org/data/2.5/weather?lat=59.4372&lon=24.7453&appid=3cfcda1c67930c9db4bb6363b4d06023"
+					// Tallinn, Tartu, Narva, Pärnu, Põltsamaa
+					"http://api.openweathermap.org/data/2.5/group?id=588409,588335,590031,589580,589379 &units=metric&appid=3cfcda1c67930c9db4bb6363b4d06023"
+				);
 
 				if (!response.ok) {
 					throw new Error("Network response was not ok");
