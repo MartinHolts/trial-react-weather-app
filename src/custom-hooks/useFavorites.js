b4f0707f7adf7d5f7693a88c93d4d9f22e856c4e
MartinHolts <martinholtsmeier@gmail.com>
2024-05-19 18:58:31 +0300

    Changed comments.

--- src/custom-hooks/useFavorites.js
+++ src/custom-hooks/useFavorites.js
@@ -3,17 +3,18 @@ import { useState } from "react";
 function useFavorites() {
 	var [favorites, setFavorites] = useState([]);
 
-	// addToFavorites function takes a cityId as an argument and adds it to the favorites array. It creates a new array that includes all previous favorites plus the newly added city ID.
+	// addToFavorites function takes a cityId as an argument and adds it to the favorites array.
 	// old state value of favorites is used as new input to inner function as prevFavorites.
 	function addToFavorites(cityId) {
 		setFavorites(
+			// Creates a new array that includes all previous favorites plus the newly added city ID.
 			function (prevFavorites) {
 				return [...prevFavorites, cityId];
 			}
 		);
 	}
 
-	// The removeFromFavorites function takes a cityId as an argument and removes it from the favorites array. It filters out the city ID from the current list of favorites.
+	// The removeFromFavorites function takes a cityId as an argument and removes it from the favorites array.
 	// old state value of favorites is used as new input to inner function as prevFavorites.
 	function removeFromFavorites(cityId) {
 		setFavorites(
