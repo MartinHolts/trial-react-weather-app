64ae271e6111cc80f5515bc62fe1149f67ecf05f
MartinHolts <martinholtsmeier@gmail.com>
2024-05-20 12:56:10 +0300

    refactored.

--- src/components/WeatherDataDisplay/WeatherDataDisplay.js
+++ src/components/WeatherDataDisplay/WeatherDataDisplay.js
@@ -18,7 +18,7 @@ const WeatherDataDisplay = () => {
 						<p>Feels Like: {city.main.feels_like}°C</p>
 						<p>Humidity: {city.main.humidity}%</p>
 						<p>Wind Speed: {city.wind.speed} m/s</p>
-						<button onClick={() => addToFavorites(city.id)}>
+						<button onClick={function () { addToFavorites(city.id) }}>
 							Add to Favorites
 						</button>
 					</div>
