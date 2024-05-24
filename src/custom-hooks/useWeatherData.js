7eab0cfa900fa8ad0e7c6e7d1cb9bce132dc1d24
MartinHolts <martinholtsmeier@gmail.com>
2024-05-24 17:07:28 +0300

    Updated code to save fetched data in localstorage so it wont be fetched again on each page reload.

--- src/custom-hooks/useWeatherData.js
+++ src/custom-hooks/useWeatherData.js
@@ -1,9 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 const useWeatherData = function () {
-	const [data, setData] = useState("");
+	// Local Storage: The useState hook is initialized with a function that retrieves data from localStorage if it exists. If not, it defaults to an empty string.
+	const [data, setData] = useState(() => {
+		const savedData = localStorage.getItem("weatherData");
+		return savedData ? JSON.parse(savedData) : "";
+	});
 
-	useEffect(function () {
+	useEffect(() => {
 		async function fetchData() {
 			try {
 				const apiKey = "3cfcda1c67930c9db4bb6363b4d06023";
@@ -11,8 +15,8 @@ const useWeatherData = function () {
 					"588409",
 					"588335",
 					"590031",
-					//"589580",
-					//"589379",
+					// "589580",
+					// "589379",
 				]; // Tallinn, Tartu, Narva, Pärnu, Põltsamaa
 
 				// Fetches weather data for a group of cities identified by cityIds.
@@ -31,28 +35,31 @@ const useWeatherData = function () {
 				// Parses the JSON response body into a JavaScript object containing the weather data for the specified cities.
 				const data = await response.json();
 				setData(data);
+				// Saving to Local Storage: When new data is fetched, it is saved to localStorage.
+				localStorage.setItem("weatherData", JSON.stringify(data));
 			} catch (error) {
-				console.error(
-					"There has been a problem with your fetch operation:",
-					error
-				);
+				console.error("There has been a problem with your fetch operation:", error);
 			}
 		}
 
-		// Call fetchData initially
-		fetchData();
+		// Data Fetching: The useEffect fetches data only if data is empty (i.e., no data in local storage).
+		if (!data) {
+			fetchData();
+		}
 
 		// Set up an interval to refetch data every 300 seconds (5 minutes). This way 1000 API calls limit lasts 14 hours.
 		const intervalId = setInterval(fetchData, 300000);
 
 		// On cleanup run the function to Clear the interval.
-		return function () {
+		return () => {
 			clearInterval(intervalId);
-		}
+		};
+	}, [data]);
 
-	}, []);
+	// Memoization: useMemo is used to memoize the data.
+	const memoizedData = useMemo(() => data, [data]);
 
-	return [data];
+	return [memoizedData];
 };
 
 export default useWeatherData;
