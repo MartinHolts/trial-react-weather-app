18e17c853d5e296644d6d96f54861d7606ee3de3
MartinHolts <martinholtsmeier@gmail.com>
2024-05-25 01:33:04 +0300

    Refactored to use normal functions.

--- src/custom-hooks/useFavorites.js
+++ src/custom-hooks/useFavorites.js
@@ -2,26 +2,34 @@ import { useEffect, useState } from "react";
 
 function useFavorites() {
 	// Initialize state from localStorage or empty array if nothing found
-	const [favorites, setFavorites] = useState(() => JSON.parse(localStorage.getItem('favorites')) || []);
+	const [favorites, setFavorites] = useState(function () {
+		return JSON.parse(localStorage.getItem('favorites')) || [];
+	});
 
 	// Effect to synchronize state with localStorage
-	useEffect(() => {
+	useEffect(function () {
 		localStorage.setItem('favorites', JSON.stringify(favorites));
 	}, [favorites]);
 
 	// Function to add a cityId to the favorites array
 	function addToFavorites(cityId) {
 		if (!favorites.includes(cityId)) {
-			setFavorites(prevFavorites => [...prevFavorites, cityId]);
+			setFavorites(function (prevFavorites) {
+				return [...prevFavorites, cityId];
+			});
 		}
 	}
 
 	// Function to remove a cityId from the favorites array
 	function removeFromFavorites(cityId) {
-		setFavorites(prevFavorites => prevFavorites.filter(id => id !== cityId));
+		setFavorites(function (prevFavorites) {
+			return prevFavorites.filter(function (id) {
+				return id !== cityId;
+			});
+		});
 	}
 
-	return { favorites, addToFavorites, removeFromFavorites };
+	return { favorites: favorites, addToFavorites: addToFavorites, removeFromFavorites: removeFromFavorites };
 }
 
 export default useFavorites;
