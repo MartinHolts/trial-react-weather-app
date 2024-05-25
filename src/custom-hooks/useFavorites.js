6ecf35eae8233ef587c2844aeb929eed47d218dd
MartinHolts <martinholtsmeier@gmail.com>
2024-05-25 23:18:42 +0300

    Added presort to under useFavorites hook and removed same sort from FavoritCitiesDisplay page.

--- src/custom-hooks/useFavorites.js
+++ src/custom-hooks/useFavorites.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import useWeatherData from './useWeatherData';
 
 function useFavorites() {
+	const [fetchedData] = useWeatherData();
+
 	// Initialize state from localStorage or empty array if nothing found
 	const [favorites, setFavorites] = useState(function () {
 		return JSON.parse(localStorage.getItem('favorites')) || [];
@@ -11,11 +14,22 @@ function useFavorites() {
 		localStorage.setItem('favorites', JSON.stringify(favorites));
 	}, [favorites]);
 
-	// Function to add a cityId to the favorites array
+	// Function to add a cityId to the favorites array and sort it based on the fetchedData order
 	function addToFavorites(cityId) {
 		if (!favorites.includes(cityId)) {
 			setFavorites(function (prevFavorites) {
-				return [...prevFavorites, cityId];
+				const updatedFavorites = [...prevFavorites, cityId];
+
+				// Sort the updated favorites based on the fetchedData order
+				if (fetchedData && fetchedData.list) {
+					updatedFavorites.sort(function (a, b) {
+						const indexA = fetchedData.list.findIndex(city => city.id === a);
+						const indexB = fetchedData.list.findIndex(city => city.id === b);
+						return indexA - indexB;
+					});
+				}
+
+				return updatedFavorites;
 			});
 		}
 	}
